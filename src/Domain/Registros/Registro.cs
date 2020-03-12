9ff62642c79620c34af453573e317cd73f4e0a16
Gustavo Murayama <gmurayama@outlook.com>
2020-03-12 19:38:51 -0300

    New field added to Registro object

--- src/Domain/Registros/Registro.cs
+++ src/Domain/Registros/Registro.cs
@@ -10,6 +10,8 @@ namespace Truckmanager.Domain
 
         public bool EstaCarregado { get; set; }
 
+        public TipoCaminhao TipoCaminhao { get; set; }
+
         public Local Origem { get; set; }
 
         public Local Destino { get; set; }
