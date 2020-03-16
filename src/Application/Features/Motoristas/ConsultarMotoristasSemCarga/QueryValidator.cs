97c9a5dce6c30f5b1868d2f92b8ddaa623a84be3
Gustavo Murayama <gmurayama@outlook.com>
2020-03-16 17:22:49 -0300

    Added missing QueryValidator

---
+++ src/Application/Features/Motoristas/ConsultarMotoristasSemCarga/QueryValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+
+namespace TruckManager.Application.Features.Motoristas
+{
+    public partial class ConsultarMotoristasSemCarga
+    {
+        public class QueryValidator : AbstractValidator<Query>
+        {
+            public QueryValidator()
+            {
+                RuleFor(x => x.DataInicial)
+                    .GreaterThanOrEqualTo(new DateTime(1753, 1, 1)) // SQL Server DateTime MinValue
+                        .WithMessage("Necessário especificar uma data válida maior que 1 de Janeiro de 1753");
+
+                RuleFor(x => x.DataFinal)
+                   .GreaterThanOrEqualTo(new DateTime(1753, 1, 1)) // SQL Server DateTime MinValue
+                       .WithMessage("Necessário especificar uma data válida maior que 1 de Janeiro de 1753")
+                   .GreaterThan(x => x.DataInicial)
+                       .WithMessage("Informar uma data final maior que a data inicial");
+            }
+        }
+    }
+}
